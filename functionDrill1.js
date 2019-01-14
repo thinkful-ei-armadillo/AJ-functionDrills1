b72728eaa846777bab2d2e1d5b8f45f3ab089ace
Jonathan Kleinpeter <joklein341@gmail.com>
2019-01-14 15:12:51 -0600

    add seperate function for yob calculation

--- functionDrill1.js
+++ functionDrill1.js
@@ -1,6 +1,10 @@
+function getYearOfBirth(age) {
+	return 2000 + (19 - age);
+}
+
 function createGreeting(name, age) {
-	const yearOfBirth = 2000 + (16 - age);
-	return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
+	const yob = getYearOfBirth(age);
+	return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`;
 }
 
 const greeting = createGreeting("Jonathan", 3);
