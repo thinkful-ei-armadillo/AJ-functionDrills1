ea0b4150732573fb43df60c2ad97b9c3cfd40107
Jonathan Kleinpeter <joklein341@gmail.com>
2019-01-14 15:33:24 -0600

    final commit

--- functionDrill1.js
+++ functionDrill1.js
@@ -2,11 +2,17 @@ function getYearOfBirth(age) {
 	if (age < 0) {
 		throw new Error("Age can not be negative");
 	}
+	if (typeOf(age) === 'number') {
+		throw new Error('Age must be a number');
+	}
 	return 2000 + (19 - age);
 }
 
 function createGreeting(name, age) {
 		const yob = getYearOfBirth(age);
+		if (typeOf(name) !== 'string') {
+			throw new Error('Name must be a string');
+		}
 		return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`;
 }
 
