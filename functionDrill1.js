86615d5f9168b200c6c7f80bbdfb68e6b8848e40
Ali Lahrime <alahrime@email.wm.edu>
2019-01-14 16:21:03 -0500

    Put in some error catching

--- functionDrill1.js
+++ functionDrill1.js
@@ -1,11 +1,17 @@
 function getYearOfBirth(age) {
+	if (age < 0) {
+		throw new Error("Age can not be negative");
+	}
 	return 2000 + (19 - age);
 }
 
 function createGreeting(name, age) {
-	const yob = getYearOfBirth(age);
-	return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`;
+		const yob = getYearOfBirth(age);
+		return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`;
 }
 
-const greeting = createGreeting("Jonathan", 3);
-console.log(greeting);
+try {
+	const greeting = createGreeting("Jonathan", -3);
+} catch(err) {
+	console.log("Error");
+}
